273e3966803b652690615e03da2fb34cd0cc06e5
Philipp Mos <account@philipp-moser.de>
2022-07-12 21:53:03 +0200

    Add some stuff to get BlobUrl

--- src/MoserBlog.MediaTool.Api/Program.cs
+++ src/MoserBlog.MediaTool.Api/Program.cs
@@ -1,3 +1,5 @@
+using Azure.Storage.Blobs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -10,6 +12,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/image", (string imageName) => {
+    var blobServiceClient = new BlobServiceClient(builder.Configuration.GetConnectionString("BlobConnection"));
+    var containerClient = blobServiceClient.GetBlobContainerClient(builder.Configuration["BlobConfig:ContainerName"]);
+
+    var blobClient = containerClient.GetBlobClient(imageName);
+
+    return $"{containerClient.Uri}/{blobClient.Name}";
+});
 
 app.UseHttpsRedirection();
 
